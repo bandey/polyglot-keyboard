5f7a3d14d92b9750053c921474730ef7ec643f3c
bandey <origbandey@gmail.com>
2018-10-11 20:03:23 +0300

    Separate common lines in layout

--- src/components/PolyglotKeyboard/keyboardLayout.js
+++ src/components/PolyglotKeyboard/keyboardLayout.js
@@ -1,40 +1,43 @@
+const commonFirstLine = '{accept} {cancel} {meta-lat} {meta-gre}';
+const commonSecondPart = '{alt} {shift} {bksp} {space}';
+
 export default {
   name: 'custom',
   layout: {
     'normal': [
-      '{accept} {cancel} {meta-lat} {meta-gre}',
+      commonFirstLine,
     ],
     'meta-lat' : [ // Latin lower case
-      '{accept} {cancel} {meta-lat} {meta-gre}',
-      '{alt} {shift} {bksp} {space} a b c d e f g h i j k l m n o p q r s t u w v x y z',
+      commonFirstLine,
+      commonSecondPart + ' a b c d e f g h i j k l m n o p q r s t u w v x y z',
     ],
     'meta-lat-shift' : [ // Latin upper case
-      '{accept} {cancel} {meta-lat} {meta-gre}',
-      '{alt} {shift} {bksp} {space} A B C D E F G H I J K L M N O P Q R S T U W V X Y Z',
+      commonFirstLine,
+      commonSecondPart + ' A B C D E F G H I J K L M N O P Q R S T U W V X Y Z',
     ],
     'meta-lat-alt' : [ // Latin punctuation, digits and symbols part 1
-      '{accept} {cancel} {meta-lat} {meta-gre}',
-      '{alt} {shift} {bksp} {space} \u2014 ` 1 2 3 4 5 6 7 8 9 0 - = / \\ [ ] , . ; \' \u2018 \u2019',
+      commonFirstLine,
+      commonSecondPart + ' \u2014 ` 1 2 3 4 5 6 7 8 9 0 - = / \\ [ ] , . ; \' \u2018 \u2019',
     ],
     'meta-lat-alt-shift' : [ // Latin punctuation, digits and symbols part 2
-      '{accept} {cancel} {meta-lat} {meta-gre}',
-      '{alt} {shift} {bksp} {space} \u2116 ~ ! @ # $ % ^ & * ( ) _ + ? | { } < > : " \u201c \u201d \u00ab \u00bb',
+      commonFirstLine,
+      commonSecondPart + ' \u2116 ~ ! @ # $ % ^ & * ( ) _ + ? | { } < > : " \u201c \u201d \u00ab \u00bb',
     ],
     'meta-gre' : [ // Greek lower case
-      '{accept} {cancel} {meta-lat} {meta-gre}',
-      '{alt} {shift} {bksp} {space} \u03b1 \u03b2 \u03b3 \u03b4 \u03b5 \u03b6 \u03b7 \u03b8 \u03b9 \u03ba \u03bb \u03bc \u03bd \u03be \u03bf \u03c0 \u03c1 \u03c2 \u03c3 \u03c4 \u03c5 \u03c6 \u03c7 \u03c8 \u03c9',
+      commonFirstLine,
+      commonSecondPart + ' \u03b1 \u03b2 \u03b3 \u03b4 \u03b5 \u03b6 \u03b7 \u03b8 \u03b9 \u03ba \u03bb \u03bc \u03bd \u03be \u03bf \u03c0 \u03c1 \u03c2 \u03c3 \u03c4 \u03c5 \u03c6 \u03c7 \u03c8 \u03c9',
     ],
     'meta-gre-shift' : [ // Greek upper case
-      '{accept} {cancel} {meta-lat} {meta-gre}',
-      '{alt} {shift} {bksp} {space} \u0391 \u0392 \u0393 \u0394 \u0395 \u0396 \u0397 \u0398 \u0399 \u039a \u039b \u039c \u039d \u039e \u039f \u03a0 \u03a1 \u03a3 \u03a4 \u03a5 \u03a6 \u03a7 \u03a8 \u03a9',
+      commonFirstLine,
+      commonSecondPart + ' \u0391 \u0392 \u0393 \u0394 \u0395 \u0396 \u0397 \u0398 \u0399 \u039a \u039b \u039c \u039d \u039e \u039f \u03a0 \u03a1 \u03a3 \u03a4 \u03a5 \u03a6 \u03a7 \u03a8 \u03a9',
     ],
     'meta-gre-alt' : [ // Greek diacritics and symbols
-      '{accept} {cancel} {meta-lat} {meta-gre}',
-      '{alt} {shift} {bksp} {space} \u03de \u03e0 \u271d \u02bc \u0375 \u0384 \u25cc\u0345 \u25cc\u0313 \u25cc\u0314 \u25cc\u0308 \u25cc\u0342 \u25cc\u0300 \u25cc\u0301 \u25cc\u0304 \u0387',
+      commonFirstLine,
+      commonSecondPart + ' \u03de \u03e0 \u271d \u02bc \u0375 \u0384 \u25cc\u0345 \u25cc\u0313 \u25cc\u0314 \u25cc\u0308 \u25cc\u0342 \u25cc\u0300 \u25cc\u0301 \u25cc\u0304 \u0387',
     ],
     'meta-gre-alt-shift' : [ // Greek diacritics and symbols repeat
-      '{accept} {cancel} {meta-lat} {meta-gre}',
-      '{alt} {shift} {bksp} {space} \u03de \u03e0 \u271d \u02bc \u0375 \u0384 \u25cc\u0345 \u25cc\u0313 \u25cc\u0314 \u25cc\u0308 \u25cc\u0342 \u25cc\u0300 \u25cc\u0301 \u25cc\u0304 \u0387',
+      commonFirstLine,
+      commonSecondPart + ' \u03de \u03e0 \u271d \u02bc \u0375 \u0384 \u25cc\u0345 \u25cc\u0313 \u25cc\u0314 \u25cc\u0308 \u25cc\u0342 \u25cc\u0300 \u25cc\u0301 \u25cc\u0304 \u0387',
     ],
   },
   display: {
